a79bc03a433cea85ab3140d622c2fc42a440436b
aorange <1041249649@qq.com>
2020-04-11 16:01:39 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">我是你们的经理初始化的项目</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
