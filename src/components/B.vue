a2bb66a290dfaa42fb918ab12a67110aa326394a
aorange <1041249649@qq.com>
2020-04-11 16:39:00 +0800

    程序员B的第一天的劳动成果

---
+++ src/components/B.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='B'>
+      我是程序员B第一天写的代码
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'B',
+  data(){
+    return {}
+  },
+  components: {},
+  methods: {}
+}
+</script>
+<style lang='less' scoped>
+
+</style>
